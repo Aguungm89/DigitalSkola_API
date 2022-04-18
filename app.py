6e5d5d61bef50e31608a47c6dab4aa8f91fa77b0
Aguungm89 <aguungm89@gmail.com>
2022-04-18 21:08:59 +0700

    update

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from db.query_raw import connection
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+@app.route("/users", methods=["GET", "POST"])
 
 def say_hello():
     return jsonify({"text": "Hello World!"})
+
+def users():
+    if request.method == 'GET':
+        query = """
+        SELECT * FROM agung.users
+        """
+        conn, cursor = connection()
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return jsonify(result)
+
+    elif request.method == 'POST':
+        name = request.args.get('name')
+        address = request.args.get('address')
+        query = f"""
+        INSERT INTO agung.users (name, address)
+        VALUES ('{name}', '{address}')
+        """
+        conn, cursor = connection()
+        cursor.execute(query)
+        conn.commit()
+        return {"message": f"Users {name} has been created successfully."}
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
